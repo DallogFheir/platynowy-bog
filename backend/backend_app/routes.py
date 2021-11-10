ce5b51174c7d72d75418eb8a90974666c057ec02
DallogFheir <dallog.fheir@gmail.com>
2021-11-10 20:11:52 +0100

    small fix to error page

--- backend/backend_app/routes.py
+++ backend/backend_app/routes.py
@@ -28,16 +28,28 @@ def apidocs():
 def error_404(error):
     error_code = error.code
     message = "Nie znaleziono strony."
+    error_trinket = "iVBORw0KGgoAAAANSUhEUgAAAC0AAAATCAYAAAAEaoRHAAAAlElEQVR4nN2WQQ6AMAgErfFB/v9z9dQe1uCyAZvQvSkIMqC03YesDtdNDxGLeyYlXKrrw/YXUZaHvsc2pFnlTOx51jHa0W1Io0bl6uxl+80OlCetzjLrAM5mdNanypO2ZBHyEsb7aryXvSzp6H95tepuRPbVstlFewe7FcfKS/3KklaFlTPi6dqWtPfs4d2A3s1n+j2IJB1ktIHadgAAAABJRU5ErkJggg=="
 
-    return render_template("error.html", error_code=error_code, message=message), 404
+    return (
+        render_template(
+            "error.html", error_code=error_code, message=message, icon=error_trinket
+        ),
+        404,
+    )
 
 
 @app.errorhandler(500)
 def error_500(error):
     error_code = error.code
     message = "Wystąpił błąd ze strony serwera. Spróbuj ponownie później."
+    broken_modem = "iVBORw0KGgoAAAANSUhEUgAAADUAAAAvCAYAAABDq4KNAAAB5klEQVR4nO2ZO07DQBCG/0UpCBJCQjwUiYIiDRegQdyAguNAhahANFyFghtENHAAmpRIEQ/RUERUS/VH9tibWa/XDlr7q7xydrKZT7szsY21FlUYGpObMLfWVArQAmurXkATGF9TNHR1cwsAuL68yN3/T8Z6U9mxy5hkFQa7bYpIY760aSxJU4OqE3wzHmo0Br2pZbjM9KdfJAqmYu0FlyEtfgyzSZpa1Km6hrQMNx0/S5Km1NOvqbp0dHwCANjZ2wUAfH18AgBen58qxS9bX5KmvPeUzEisU1Iak2gGy9aXtikSakDLONEyXzfO3FqTpKng3i80o5wn45C6pofG2CRNqaefrxHCTBJm9PTsPGiBLjMH43Fu/DadLq7TNLUO5AxVNeMLMx5aj2hmNBoBAGazWW78MpksPpukqcLpJzMVy5zcW9peoQFCM+T+dwsAcFfyXd0wJetGrMpPJo8PubHLjGSZGUk3TJGf73cARXNE65olvkZ8uTvcAFDca0Cippx1iqY2t/dLxy5oUDMj64wvnJftILJ0r0vXjGi4Kr+8r6GZIcn/8x3wF7JLd3UUoeZiGyHLnnKlaYoX0hhxmatLTDOStE0R7fmerzlZ6Zsw4iJJU629nXfRxJvG3lQZvubafPf7B7y+JRQK/uuwAAAAAElFTkSuQmCC"
 
-    return render_template("error.html", error_code=error_code, message=message), 500
+    return (
+        render_template(
+            "error.html", error_code=error_code, message=message, icon=broken_modem
+        ),
+        500,
+    )
 
 
 # ITEMS
