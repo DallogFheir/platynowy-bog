01ba40b6f59a76e178be2fd73df6e892fb23323e
DallogFheir <dallog.fheir@gmail.com>
2025-03-17 02:07:04 +0100

    feat: Repentance+ update 5

--- utils/pool_checker.py
+++ utils/pool_checker.py
@@ -1,6 +1,85 @@
 import json
 
 TO_IGNORE = {
+    -1: "G-FUEL",
+    6: "Number One",  # Bumbino
+    12: "Magic Mushroom",  # mushrooms
+    13: "The Virus",  # Lust
+    15: "<3",  # Gluttony
+    18: "Dollar",
+    25: "Breakfast",
+    34: "The Book of Belial",  # Judas
+    36: "The Poop",  # Blue Baby & Bumbino
+    37: "Mr. Boom",  # Wrath
+    42: "Bob's Rotten Head",  # Sloth
+    45: "Yum Heart",  # (Tainted) Magadalene & Bumbino
+    46: "Lucky Foot",  # Cain
+    49: "Shoop da Whoop",  # Envy
+    50: "Steven",
+    56: "Lemon Mishap",  # Bumbino
+    64: "Steam Sale",  # Greed & shopkeepers
+    65: "Anarchist Cookbook",  # Pride
+    71: "Mini Mush",  # mushrooms
+    73: "Cube of Meat",  # Harbingers
+    74: "A Quarter",  # Super Greed & urns
+    81: "Dead Cat",  # Super Pride
+    86: "Monstro's Tooth",  # Bumbino
+    90: "The Small Rock",  # obstacles
+    96: "Little C.H.A.D.",  # bosses
+    99: "Little Gish",  # bosses
+    100: "Little Steven",  # bosses
+    103: "The Common Cold",  # Bumbino
+    105: "The D6",
+    106: "Mr. Mega",  # bosses
+    117: "Dead Bird",
+    119: "Blood Bag",
+    122: "Whore of Babylon",
+    126: "Razor Blade",
+    128: "Forever Alone",  # bosses
+    129: "Bucket of Lard",  # bosses
+    130: "A Pony",
+    132: "A Lump of Coal",
+    135: "IV Bag",
+    140: "Bob's Curse",  # bosses
+    157: "Bloody Lust",
+    158: "Crystal Ball",
+    163: "Ghost Baby",  # obstacles
+    177: "Portable Slot",  # no Beggar nor Shop in pools in game files?
+    181: "White Pony",
+    207: "Ball of Bandages",  # bosses
+    209: "Butt Bombs",  # ignore Shell Game pool
+    218: "Placenta",  # obstacles
+    238: "Key Piece 1",
+    239: "Key Piece 2",
+    254: "Blood Clot",  # obstacles"
+    265: "Dry Baby",  # obstacles
+    270: "Leech",  # obstacles
+    293: "Head of Krampus",
+    327: "Polaroid",
+    328: "Negative",
+    349: "Wooden Nickel",
+    357: "Box of Friends",
+    378: "No. 2",  # ignore Shell Game pool
+    429: "Head of the Keeper",
+    446: "Dead Tooth",
+    453: "Compound Fracture",
+    459: "Sinus Infection",
+    474: "Broken Glass Cannon",
+    477: "Void",
+    484: "Wait What?",
+    504: "Brown Nugget",  # ignore Shell Game pool
+    576: "Dirty Mind",  # ignore Shell Game pool
+    550: "Broken Shovel",
+    551: "Broken Shovel",
+    552: "Mom's Shovel",
+    584: "Book of Virtues",
+    609: "Eternal D6",
+    626: "Knife Piece 1",
+    627: "Knife Piece 2",
+    633: "Dogma",
+    650: "Plum Flute",
+    668: "Dad's Note",
+    673: "Redemption",
     688: "Inner Child",
     705: "Dark Arts",
     706: "Abyss",
